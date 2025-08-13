e901d6f7255318103ae9e6dafbda84f0ca578140
Arianna <ariigrangetto5@gmail.com>
2025-08-13 20:05:49 -0300

    separete constants

---
+++ src/constanst.js
@@ -0,0 +1,26 @@
+export const DEFAULT_SOURCE_LANGUAGE = "es";
+export const DEFAULT_TARGET_LANGUAGE = "en";
+
+export const SUPPORTED_LANGUAGES = [
+  "en",
+  "es",
+  "fr",
+  "de",
+  "it",
+  "pt",
+  "ru",
+  "ja",
+  "zh",
+];
+
+export const FULL_LANGUAGES_CODES = {
+  es: "es-ES",
+  en: "en-US",
+  fr: "fr-FR",
+  de: "de-DE",
+  it: "it-IT",
+  pt: "pt-PT",
+  ru: "ru-RU",
+  ja: "ja-JP",
+  zh: "zh-CN",
+};
